b4bb52fcb2716cef5a6f5aff83baa0edfe7ab149
watase-eth <seankwatase@gmail.com>
2024-03-28 10:17:02 -1000

    Add .env exmaple and update Auth

--- pages/api/auth/[...thirdweb].ts
+++ pages/api/auth/[...thirdweb].ts
@@ -4,6 +4,9 @@ import { PrivateKeyWallet } from '@thirdweb-dev/auth/evm';
 export const { ThirdwebAuthHandler, getUser } = ThirdwebAuth({
     wallet: new PrivateKeyWallet(process.env.PRIVATE_KEY || ""),
     domain: process.env.DOMAIN || "",
+    thirdwebAuthOptions: {
+        secretKey: process.env.THIRDWEB_AUTH_SECRET_KEY,
+    },
 });
 
 export default ThirdwebAuthHandler();
